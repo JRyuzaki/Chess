d7075e581d001143af8529bad74fa0e7089dd372
Giorgos <giorgozombo@yahoo.gr>
2016-09-11 10:49:17 +0300

    moves code cleanup

--- src/pieces/impl/Rook.java
+++ src/pieces/impl/Rook.java
@@ -11,83 +11,49 @@ import pieces.PieceType;
 import pieces.Player;
 import util.Position;
 
-public class Rook extends AbstractPiece{
+public class Rook extends AbstractPiece {
 
-	public Rook(Player player) {
-		super(PieceType.ROOK, player);
-	}
+		public Rook(Player player) {
+			super(PieceType.ROOK, player);
+		}
 
-	@Override
-	public List<Move> getMoves(ChessBoard chessboard, Position origin) {
-		List<Move> moves = new ArrayList<>();
-		AbstractPiece movingPiece = chessboard.getPiece(origin);
+		@Override
+		public List<Move> getMoves(ChessBoard chessboard, Position origin) {
+			List<Move> moves = new ArrayList<>();
+			AbstractPiece movingPiece = chessboard.getPiece(origin);
+			for (int y = -1; y <= 1; y++) {
+				for (int x = -1; x <= 1; x++) {
+					if ( !((y == 0) ^ (x == 0)) ) continue;
+					for (int i = 1; i < 8; i++) {
+						Position current = new Position(origin.x + x * i, origin.y + y * i);
+						if (ChessBoard.isOutOfBounds(current)) break;
+						AbstractPiece piece = chessboard.getPiece(current);
+						if (piece == null) {
+							moves.add(new Move(movingPiece, origin, current));
+						} else {
+							if (piece.getPlayer() != movingPiece.getPlayer()) moves.add(new Capture(movingPiece, origin, current, piece));
+							break;
+						}
+					}
+				}
+			}
 
-		// right moves
-		for (int i = 1; i < 8; i++) {
-			Position current = new Position(origin.x + i, origin.y);
-			if (ChessBoard.isOutOfBounds(current)) break;
-			AbstractPiece piece = chessboard.getPiece(current);
-			
-			if (piece == null) {
-				moves.add(new Move(movingPiece, origin, current));
-			}else{
-				if (piece.getPlayer() != movingPiece.getPlayer()) moves.add(new Capture(movingPiece, origin, current, piece));
+			return moves;
+		}
+
+		@Override
+		public String toString() {
+			String visualization = "~";
+
+			switch (this.getPlayer()) {
+			case PLAYER_ONE:
+				visualization = "R";
+				break;
+			case PLAYER_TWO:
+				visualization = "r";
 				break;
 			}
-		}
-		// left moves
-		for (int i = 1; i < 8; i++) {
-			Position current = new Position(origin.x - i, origin.y);
-			if (ChessBoard.isOutOfBounds(current)) break;
-			AbstractPiece piece = chessboard.getPiece(current);
-			if (piece == null) {
-				moves.add(new Move(movingPiece, origin, current));
-			}else{
-				if (piece.getPlayer() != movingPiece.getPlayer()) moves.add(new Capture(movingPiece, origin, current, piece));
-				break;
-			}
-		}
-		// up moves
-		for (int i = 1; i < 8; i++) {
-			Position current = new Position(origin.x, origin.y - i);
-			if (ChessBoard.isOutOfBounds(current)) break;
-			AbstractPiece piece = chessboard.getPiece(current);
-			if (piece == null) {
-				moves.add(new Move(movingPiece, origin, current));
-			}else{
-				if (piece.getPlayer() != movingPiece.getPlayer()) moves.add(new Capture(movingPiece, origin, current, piece));
-				break;
-			}
-		}
-		// down moves
-		for (int i = 1; i < 8; i++) {
-			Position current = new Position(origin.x, origin.y + i);
-			if (ChessBoard.isOutOfBounds(current)) break;
-			AbstractPiece piece = chessboard.getPiece(current);
-			if (piece == null) {
-				moves.add(new Move(movingPiece, origin, current));
-			}else{
-				if (piece.getPlayer() != movingPiece.getPlayer()) moves.add(new Capture(movingPiece, origin, current, piece));
-				break;
-			}
-		}
 
-		return moves;
-	}
-
-	@Override
-	public String toString() {
-		String visualization = "~";
-		
-		switch(this.getPlayer()){
-		case PLAYER_ONE:
-			visualization = "R";
-			break;
-		case PLAYER_TWO:
-			visualization = "r";
-			break;
+			return visualization;
 		}
-		
-		return visualization;
-	}
 }
